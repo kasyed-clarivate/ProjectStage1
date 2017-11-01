3183b4afda729a24485f90ed7a8d49088b94db57
Ali.Syed <emailsyed244@gmail.com>
2017-11-01 16:33:24 +0000

    Added some comments

--- src/main/java/com/stage1/test/configuration/WaitPage.java
+++ src/main/java/com/stage1/test/configuration/WaitPage.java
@@ -16,64 +16,129 @@ public class WaitPage {
     @Autowired
     private WebConnect driver;
 
+    /**
+     * This simple method returns the WebElement when supplied By locator to it.
+     *
+     * @param by
+     * @return WebElement
+     */
     protected WebElement getElement(By by){
         return driver.getDriver().findElement(by);
     }
 
-    protected WebDriver getDriver() {
+   protected WebDriver getDriver() {
         return driver.getDriver();
     }
 
+    /**
+     * This wait method returns the List of WebElements present in the DOM when supplied By locator to it.
+     *
+     * @param by
+     * @return List<WebElement>
+     */
     protected List<WebElement> waitForExpectedElements(final By by) {
         Wait<WebDriver> wait = new WebDriverWait(getDriver(), WAIT_TIME);
         wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(by));
         return getDriver().findElements(by);
     }
 
+    /**
+     * This wait method returns the List of WebElements present in the DOM when supplied By locator to it along with wait.
+     *
+     * @param by,int
+     * @return List<WebElement>
+     */
     protected List<WebElement> waitForExpectedElements(final By by, int timeout) {
         Wait<WebDriver> wait = new WebDriverWait(getDriver(), timeout);
         wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(by));
         return getDriver().findElements(by);
     }
 
+    /**
+     * This wait method returns the List of WebElements Visible to User in the DOM when supplied By locator to it.
+     *
+     * @param by
+     * @return List<WebElement>
+     */
     protected List<WebElement> waitForElementsVisible(final By by) {
         Wait<WebDriver> wait = new WebDriverWait(getDriver(), WAIT_TIME);
         wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(by));
         return getDriver().findElements(by);
     }
 
+    /**
+     * This wait method returns the List of WebElements present in the DOM when supplied By locator to it along with wait.
+     *
+     * @param by
+     * @return List<WebElement>
+     */
     protected List<WebElement> waitForElementsVisible(final By by, int timeout) {
         Wait<WebDriver> wait = new WebDriverWait(getDriver(), timeout);
         return wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(by));
     }
 
+    /**
+     * This wait method returns the List of WebElements present in the DOM when supplied By locator to it.
+     *
+     * @param by
+     * @return List<WebElement>
+     */
     protected WebElement waitForElementPresent(final By by) {
         Wait<WebDriver> wait = new WebDriverWait(getDriver(), WAIT_TIME);
         return wait.until(ExpectedConditions.presenceOfElementLocated(by));
     }
+
+    /**
+     * This wait method returns the WebElement present in the DOM when supplied By locator to it along with wait in int.
+     *
+     * @param by,int
+     * @return WebElement
+     */
 
     protected WebElement waitForElementPresent(final By by, int timeout) {
         Wait<WebDriver> wait = new WebDriverWait(getDriver(), timeout);
         return wait.until(ExpectedConditions.presenceOfElementLocated(by));
     }
 
+    /**
+     * This wait method returns the WebElement visible to user in the DOM when supplied By locator to it
+     *
+     * @param by
+     * @return WebElement
+     */
     protected WebElement waitForElementVisible(final By by) {
         Wait<WebDriver> wait = new WebDriverWait(getDriver(), WAIT_TIME);
         wait.until(ExpectedConditions.visibilityOfElementLocated(by));
         return getDriver().findElement(by);
     }
 
+    /**
+     * This wait method returns the WebElement visible in the DOM when supplied By locator to it along with wait in int.
+     *
+     * @param by,int
+     * @return WebElement
+     */
     protected WebElement waitForElementVisible(final By by, int timeout) {
         Wait<WebDriver> wait = new WebDriverWait(getDriver(), timeout);
         wait.until(ExpectedConditions.visibilityOfElementLocated(by));
         return getDriver().findElement(by);
     }
 
+    /**
+     * This methods navigates the browser to the given url.
+     *
+     * @param url
+     * @return void
+     */
     protected void goTo(String url) {
         getDriver().navigate().to(url);
     }
 
-
+    /**
+     * This method maximises the browser window.
+     *
+     * @return void
+     */
     protected void maximizeWindow() {
         getDriver().manage().window().maximize();
     }
